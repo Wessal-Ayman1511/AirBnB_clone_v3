b0be94a28495db0f987b47cdbde586227c0b3d98
Wessal Ayman <wessalayman237@gmail.com>
2024-08-15 17:24:41 +0300

    stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,16 @@
 """
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'])
 def status():
     """Return status of the API"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('stats', methods=['GET'])
+def stats():
+    """Return status of the API"""
+    return jsonify(storage.count())
